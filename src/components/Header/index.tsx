68a7505aa4583333ffc5a176ffc7a2716e0d8008
Lucas Mota <lucas.o.mota@gmail.com>
2022-05-23 07:42:39 -0300

    feat(*): Criada estrutura para suportar o prismic em ambiente de desenvolvimento; Implementação do css global; Implementação da página home e componente Header; Busca e apresentação dos dados provenientes do Prismic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,9 @@
-export default function Header() {
-  // TODO
+export default function Header(): JSX.Element {
+  return (
+    <header>
+      <h1>
+        <img src="/assets/Logo.svg" alt="space traveling logo" />
+      </h1>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import { GetStaticProps } from 'next';
 
+import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +28,54 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+const formatMyDate = (date: Date): string => {
+  return format(new Date(date), '', { locale: '' });
+};
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const { results } = postsPagination;
+  return (
+    <div className={styles.container}>
+      <Header />
+      {results.map(post => {
+        return (
+          <section key={post.uid} className={styles['container-post']}>
+            <h3>{post.data.title}</h3>
+            <p>{post.data.subtitle}</p>
+            <div className={styles['container-event-info']}>
+              <div className={styles.eventInfo}>
+                <AiOutlineCalendar color="FFFFFF" />
+                <span>
+                  <time>
+                    {format(
+                      new Date(post.first_publication_date),
+                      'dd MMM yyyy',
+                      {
+                        locale: ptBR,
+                      }
+                    )}
+                  </time>
+                </span>
+              </div>
+              <div className={styles.eventInfo}>
+                <AiOutlineUser color="FFFFFF" />
+                <span>{post.data.author}</span>
+              </div>
+            </div>
+          </section>
+        );
+      })}
+    </div>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const { results } = await prismic.getByType('post-type');
+  console.log(JSON.stringify(results));
+  return {
+    props: {
+      postsPagination: { results },
+    },
+  };
+};
